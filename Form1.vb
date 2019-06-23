d9d055a270e5f166e676d1458027657c8c1cefd6
Curicows <giovane0845@gmail.com>
2019-06-23 07:53:21 -0300

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -7,4 +7,12 @@
     Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
 
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        RelayForm.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TempForm.Show()
+    End Sub
 End Class
